bed2fecdf1ae6492437cced344a7d696dde194cc
Jason Beneducci <jasonbeneducci@yahoo.com>
2019-08-01 01:46:10 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,21 +12,23 @@ def third_challenge
       :hero => {
         :name => "Romeo"
         :age => "15"
+        :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet"
-        :age => "51"
+        :age => "50"
         },
       :matriarch => {
         :name => "Lady Capulet"
-        :age => "52"
+        :age => "51"
         },
       :heroine => {
-        :name =>
-        :age =>
+        :name => "Juliet"
+        :age => "15"
+        :status => "alive"
         },
       :heroine_friends => []
    }
